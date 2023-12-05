0295ffb4cbaf6ba63acc75d3d3eb33cb53317504
TsumeKuromaki <tsumedesu1@gmail.com>
2023-12-05 02:34:04 -0300

    first commit

---
+++ testejs.js
@@ -0,0 +1,25 @@
+let q1;
+let q2;
+let anime = 24;
+let answer;
+let tanswer;
+
+document.getElementById("submit").onclick = function(){
+   q2 = document.getElementById("q1").value;
+   q2 = Number(q2);
+   answer = Math.round(q2 * anime / 60);
+   
+   if(tanswer > 24){
+
+   q1 = document.getElementById("time").value;
+   q1 = Number(q1);
+   let qa = q1 - 24;
+   Math.round(tanswer) = qa + answer;}
+   else{
+      q1 = document.getElementById("time").value;
+      q1 = Number(q1);
+      tanswer = q1 + answer;
+   }
+   document.getElementById("answer").innerHTML = "This about " + answer + "H";
+   document.getElementById("tanswer").innerHTML = "You will finish on " + tanswer + "AM";
+}
